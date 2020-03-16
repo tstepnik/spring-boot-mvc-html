902f7eda4ea4465e054f65cf7fc1475a33a34eaa
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-16 21:25:08 +0100

    spring boot mvc app(not working) with jar file and html file

---
+++ src/main/java/pl/javastart/SpringBootMvcHtmlApplication.java
@@ -0,0 +1,11 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcHtmlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcHtmlApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/javastart/springbootmvchtml/SpringBootMvcHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.springbootmvchtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
