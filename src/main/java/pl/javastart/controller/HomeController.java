12521d50eb1f9dd8eedaa876dc0d7a5c143eddee
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-17 12:52:47 +0100

    add thymeleaf dependency and return to @Controller addnotation, app is working properly now

--- src/main/java/pl/javastart/controller/HomeController.java
+++ src/main/java/pl/javastart/controller/HomeController.java
@@ -1,13 +1,12 @@
 package pl.javastart.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
         return "home";
     }
